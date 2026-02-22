fef1e2912b71b24aeaa88e97bd98b9f0eaf8b29c
premsai-noolu <premsainoolu01@gmail.com>
2026-02-22 16:30:36 +0530

    Handling Errors

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,6 +3,7 @@ import classes from './page.module.css'
 import MealsGrid from '@/components/meals/meals-grid'
 import { getMeals } from '@/lib/meals'
 import { Suspense } from 'react'
+import Error from './error'
 
 async function Meals(){
     const meals = await getMeals()
