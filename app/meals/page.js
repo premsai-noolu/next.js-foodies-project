9d95f8bd92baae2dec9a4c9e2af8c5ca4e687f78
premsai-noolu <premsainoolu01@gmail.com>
2026-02-22 14:51:29 +0530

    SQLite Database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,26 @@
-export default function MealsPage(){
-    return (<main>
-        <h1>Meals Page</h1>
-    </main>)
+import Link from 'next/link'
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals'
+
+export default async function MealsPage(){
+    const meals = await getMeals();
+    return (
+        <>
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created{' '}<span className={classes.highlight}>by You</span>
+            </h1>
+            <p>Choose your favourite recipe and cook it yourself. It is easy and fun</p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">
+                    Share Your Favourite Recipe
+                </Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={meals}/>
+        </main>
+        </>
+    )
 }
